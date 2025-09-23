c33ebebeb2dbc73a9d953a58ce38e41cee38daac
Sergey Simakov <SaneniRem@yandex.ru>
2025-09-23 18:52:09 +0300

    schemas init

---
+++ app/schemas/__init__.py
@@ -0,0 +1,10 @@
+from app.schemas.question import Question, QuestionCreate, QuestionWithAnswers
+from app.schemas.answer import Answer, AnswerCreate
+
+__all__ = [
+    "Question",
+    "QuestionCreate",
+    "QuestionWithAnswers",
+    "Answer",
+    "AnswerCreate",
+]
