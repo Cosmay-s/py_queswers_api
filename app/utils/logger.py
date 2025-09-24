3961a5cdb12159c6560a74f0921b45e98f005332
Sergey Simakov <SaneniRem@yandex.ru>
2025-09-24 17:36:31 +0300

    добавил логи

--- app/utils/logger.py
+++ app/utils/logger.py
@@ -12,7 +12,10 @@ logger.remove()
 
 logger.add(
     sys.stderr,
-    format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> | <level>{level: <8}</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> - <level>{message}</level>",
+    format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> |"
+    "<level>{level: <8}</level> |"
+    "<cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan> -"
+    "<level>{message}</level>",
     level="INFO",
     colorize=True
 )
@@ -22,7 +25,8 @@ logger.add(
     f"{log_dir}/app.log",
     rotation="10 MB",
     retention="10 days",
-    format="{time:YYYY-MM-DD HH:mm:ss} | {level: <8} | {name}:{function}:{line} - {message}",
+    format="{time:YYYY-MM-DD HH:mm:ss} |"
+    "{level: <8} | {name}:{function}:{line} - {message}",
     level="DEBUG"
 )
 
